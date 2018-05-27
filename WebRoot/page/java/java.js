6172812882c4a99c21a841b2a1988c3abd5192c5
XiaoLuoA <1019558287@qq.com>
2018-05-27 19:48:56 +0800

    项目构建2

---
+++ WebRoot/page/java/java.js
@@ -0,0 +1,33 @@
+var protocol = 'ws'; // ws 或 wss
+var ip = '127.0.0.1'
+var port = 9326
+
+var heartbeatTimeout = 5000; // 心跳超时时间，单位：毫秒
+var reconnInterval = 1000; // 重连间隔时间，单位：毫秒
+
+var binaryType = 'blob'; // 'blob' or 'arraybuffer';//arraybuffer是字节
+var handler = new DemoHandler()
+
+var tiows
+
+function initWs () {
+  var queryString = 'groupId=java'
+  var param = null
+  tiows = new tio.ws(protocol, ip, port, queryString, param, handler, heartbeatTimeout, reconnInterval, binaryType)
+  tiows.connect()
+}
+
+
+
+function send () {
+  var msg = document.getElementById('textId')
+ // document.getElementById('textId').value=''
+  
+  tiows.send(msg.value)
+}
+
+function clearMsg () {
+  document.getElementById('contentId').innerHTML = ''
+}
+
+initWs()
---
+++ WebRoot/page/java/javaHandler.js
@@ -0,0 +1,35 @@
+var DemoHandler = function () {
+ var name = document.getElementById("name").innerHTML;
+  this.onopen = function (event, ws) {
+   // ws.send(name+'连上了哦')
+    document.getElementById('contentId').innerHTML += '你连上了哦<br>';
+  }
+
+  /**
+   * 收到服务器发来的消息
+   * @param {*} event 
+   * @param {*} ws 
+   */
+  this.onmessage = function (event, ws) {
+    var data = event.data
+    document.getElementById('contentId').innerHTML += data + '<br>'
+  }
+
+  this.onclose = function (e, ws) {
+    // error(e, ws)
+	  
+  }
+
+  this.onerror = function (e, ws) {
+    // error(e, ws)
+  }
+
+  /**
+   * 发送心跳，本框架会自动定时调用该方法，请在该方法中发送心跳
+   * @param {*} ws 
+   */
+  this.ping = function (ws) {
+    // log("发心跳了")
+    ws.send('心跳内容')
+  }
+}
