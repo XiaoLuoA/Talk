4fe35f3340f93641285540dfc879bb89cde59f8c
XiaoLuoA <1019558287@qq.com>
2018-05-28 21:00:24 +0800

    初始化监听器

---
+++ src/com/xiaoluo/listener/StartListener.java
@@ -0,0 +1,29 @@
+package com.xiaoluo.listener;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import websocket.TalkATalkWebsocketStarter;
+
+public class StartListener implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		try {
+			TalkATalkWebsocketStarter.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
